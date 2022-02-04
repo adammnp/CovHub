8c70a82ea91ef1ae9ba6c4601794e83e33c868c1
Iliketurtle-np <88299611+Iliketurtle-np@users.noreply.github.com>
2022-02-05 00:36:50 +0800

    javascript

--- Buttonscript.js
+++ Buttonscript.js
@@ -1,31 +1,5 @@
 
-function likedbleh(postid,username,Content,numberoflikes,objectid)
-    {
-        var jsondata =
-        {
-            "PostID":postid,
-            "Username":`"${username}"`,
-            "PostContent":`"${Content}"`,
-            "NumberOfLikes":numberoflikes+1
-        }
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url":`"https://interactivedevweek12-0d3a.restdb.io/rest/covid-thread/${objectid}"`,
-            "method": "PUT",
-            "headers": {
-              "content-type": "application/json",
-              "x-apikey": "61d256e9ccd0211b320894a3",
-              "cache-control": "no-cache"
-            },
-            "processData": false,
-            "data": JSON.stringify(jsondata)
-          }
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-        });
 
-    }
 $(document).ready(function () {
     
     const APIKEY = "61d256e9ccd0211b320894a3";
@@ -56,15 +30,53 @@ $(document).ready(function () {
                 <div class="opacity-100">${response[i].Username}</div>
                 <div class="opacity-100">${response[i].PostContent}</div>
                 <div class="opacity-100 likes">${response[i].NumberOfLikes}</div>
-                <a href="#" onclick="likedbleh(${postid},'${username}','${pc}',${nol},'${objectid}')" class="button"> Like </a>
+                <a href="#clickedlike" class="button" data-id='${response[i]._id}' data-user='${response[i].Username}' 
+                data-postid='${response[i].PostID}' data-content='${response[i].PostContent}'
+                data-likes='${response[i].NumberOfLikes}'> Like </a>
             </section>`
             $("#data").html(content);
                 }
             })
         
     }
-    
-    
+    $("#clickedlike").on("click", ".button", function (e) {
+        let objectid =  $(this).data("id");
+        let postid=$(this).data("postid");
+        let username=$(this).data("user");;
+        let pc=$(this).data("content");
+        let nol = $(this).data("likes");
+        likedbleh(postid,username,pc,nol,objectid);
+    })
+    function likedbleh(postid,username,Content,numberoflikes,objectid)
+    {
+        var jsondata =
+        {
+            "PostID":postid,
+            "Username":`"${username}"`,
+            "PostContent":`"${Content}"`,
+            "NumberOfLikes":numberoflikes+1
+        };
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url":`"https://interactivedevweek12-0d3a.restdb.io/rest/covid-thread/${objectid}"`,
+            "method": "PUT",
+            "headers": {
+              "content-type": "application/json",
+              "x-apikey": "61d256e9ccd0211b320894a3",
+              "cache-control": "no-cache"
+            },
+            "processData": false,
+            "data": JSON.stringify(jsondata)
+          }
+        $.ajax(settings).done(function(response) {
+            console.log(response);
+            location.reload();
+            Getdata();
+        });
+
+    }
+
                 
   
     
