029f6710c6bda7d919ff2ec387bdc13c7bc55f39
adammnp <94303754+adammnp@users.noreply.github.com>
2022-02-09 01:41:16 +0800

    Co-authored-by: Iliketurtle-np <Iliketurtle-np@users.noreply.github.com>

--- form.js
+++ form.js
@@ -1,3 +1,20 @@
+function myFunction() {
+  alert("Thank you for your feedback!");
+  var animation = document.getElementById('lottie');
+   animation.style.display = "block";
+   setTimeout(function(){
+    window.location.reload(1);
+ }, 1500);
+}
+const togglebutton =document.getElementById("toggle");
+const nav=document.getElementById("hehexd");
+const words=document.getElementsByClassName("wordchange");
+togglebutton.addEventListener('click',()=>{
+  nav.classList.toggle('covid-background-2');
+  nav.classList.toggle('text-white');
+})  
+
+   
 $('#swag input[required]').on('input propertychange paste change', function() {
 
     var empty = $('#swag').find('input[required]').filter(function() {
